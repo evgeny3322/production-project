b4d51d610172b8ff42b9c55a46d26406ce343092
evgeny3322 <retard_x_98@mail.ru>
2023-09-15 21:17:52 +0300

    add: i18n

--- src/shared/config/i18n/i18n.ts
+++ src/shared/config/i18n/i18n.ts
@@ -10,10 +10,13 @@ i18n
   .use(initReactI18next)
   .init({
     fallbackLng: 'ru',
-    debug: __IS_DEV__ ,
+    debug: __IS_DEV__,
 
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
+    },
+    backend: {
+      loadPath: '/locales/{{lng}}/{{ns}}.json',
     }
   });
 
