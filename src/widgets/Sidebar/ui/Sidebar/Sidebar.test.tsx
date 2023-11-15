949e68895822622ec67458f5951ee514cac9faf1
evgeny3322 <retard_x_98@mail.ru>
2023-11-15 18:41:17 +0300

    test: button,sidebar

---
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -0,0 +1,24 @@
+import { screen } from '@testing-library/react';
+import { fireEvent } from '@storybook/testing-library';
+import { Sidebar } from './Sidebar';
+import {
+    renderWithTranslation,
+} from '../../../../shared/lib/tests/renderWithTranslation/renderWithTranslation';
+
+describe('Sidebar', () => {
+    test('with only first param', () => {
+        renderWithTranslation(<Sidebar />);
+        expect(screen.getByTestId('sidebar'))
+            .toBeInTheDocument();
+    });
+
+    test('test toggle', () => {
+        renderWithTranslation(<Sidebar />);
+        const toggleBtn = screen.getByTestId('sidebar-toggle');
+        expect(screen.getByTestId('sidebar'))
+            .toBeInTheDocument();
+        fireEvent.click(toggleBtn);
+        expect(screen.getByTestId('sidebar'))
+            .toHaveClass('collapsed');
+    });
+});
